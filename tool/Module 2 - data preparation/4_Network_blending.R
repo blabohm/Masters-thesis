904504d9bf34f16ff4fb17774ae6811ce711a54e
blabohm <49242340+blabohm@users.noreply.github.com>
2022-02-15 21:43:09 +0100

    snapping function

--- tool/Module 2 - data preparation/4_Network_blending.R
+++ tool/Module 2 - data preparation/4_Network_blending.R
@@ -1,14 +1,52 @@
+################################################################################
+# MODULE 2 - DATA PREPARATION
+# PART 3 - NETWORK BLENDING
+################################################################################
 
-# 6. CONVERT TO BUILDING CENTROID
-# 7. READ OSM NETWORK
-# 8. OUTPUT TO TEMP
+# INPUT:
+# - CLEANED NETWORK (CITY LEVEL)
+# - PARK ENTRIES (CITY LEVEL)
+# - BUILDING TILES WITH POP DATA AND BUILDING ID
+# OUTPUT:
+# - BLENDED NETWORK, PARK ENTRIES, BUILDING ENTRIES
+
+################################################################################
+# OVERVIEW:
+# CONVERT TO BUILDING CENTROID
+# READ OSM NETWORK
+# OUTPUT TO TEMP
+################################################################################
+net_dir <- "D:/Berlin/network_clean1.gpkg"
+gs_dir <- "D:/Berlin/green_space_entries.gpkg"
+be_dir <- "D:/Berlin/popTiles/"
+tmpDir <- "D:/Berlin/popTilesSnapped/"
+# LOAD PACKAGES AND FUNCTIONS
+require(dplyr, quietly = TRUE)
+require(sf, quietly = TRUE)
+getwd() %>%
+  paste0("/tool/Module 2 - data preparation/functions/") %>%
+  list.files(pattern = "2-4[A-Za-z].*\\.R", full.names = TRUE) %>%
+  for (file in .) source(file)
+# READ OSM NETWORK
+OSMnetwork <- st_read(net_dir, quiet = TRUE)
+gsEntries <- st_read(gs_dir, quiet = TRUE)
 
 
-# 7. READ OSM NETWORK
-OSMnetwork <- networkLoader(net_dir)
-
-# 6. CONVERT TO BUILDING CENTROID AND SNAP TO NEAREST NETWORK LINES
+# CONVERT TO BUILDING CENTROID AND SNAP TO NEAREST NETWORK LINES
 #    -> POINT ON SURFACE
 #    -> SNAP BUILDING CENTROIDS TO NETWORK
-# 8. OUTPUT TO TEMP
-OSMbuildings_out <- OSMsnap(OSMbuildings, OSMnetwork)
+# OUTPUT TO TEMP
+be_tiles <- list.files(be_dir, pattern = ".gpkg$", full.names = TRUE)
+for (tile in be_tiles) {
+  inTile <- tile %>%
+    st_read(quiet = TRUE)
+  bbox <- sfc2bb(inTile) %>%
+    st_buffer(100)
+  net_tile <- OSMnetwork %>%
+    st_filter(bbox, .pred = st_intersects)
+
+  st_snap_points(inTile, net_tile, max_dist = 200)
+
+  #snapPTLsf(points_sf = inTile, lines_sf = net_tile, max_distance = 200) %>%
+  #  st_write(tmpDir, quiet = TRUE) #16:12
+}
--- tool/Module 2 - data preparation/functions/2-4a_Operator_functions.R
+++ tool/Module 2 - data preparation/functions/2-4a_Operator_functions.R
@@ -21,22 +21,22 @@
 ################################################################################
 # GET BBOX
 
-sfc2bb <- function(sfc_object)
-    {
-    require(sf)
-    require(dplyr)
-    bb <- st_bbox(sfc_object)
+sfc2bb <- function(sfc_object, crs = 3035)
+{
+  require(sf)
+  require(dplyr)
+  bb <- st_bbox(sfc_object)
 
-    list(rbind(c(bb$xmin, bb$ymin),
-               c(bb$xmax, bb$ymin),
-               c(bb$xmax, bb$ymax),
-               c(bb$xmin, bb$ymax),
-               c(bb$xmin, bb$ymin))) %>%
-      sf::st_polygon() %>%
-      sf::st_sfc() %>%
-      sf::st_sf() %>%
-      return()
-  }
+  list(rbind(c(bb$xmin, bb$ymin),
+             c(bb$xmax, bb$ymin),
+             c(bb$xmax, bb$ymax),
+             c(bb$xmin, bb$ymax),
+             c(bb$xmin, bb$ymin))) %>%
+    sf::st_polygon() %>%
+    sf::st_sfc() %>%
+    sf::st_sf(crs = crs) %>%
+    return()
+}
 
 
 ################################################################################
@@ -56,8 +56,7 @@ snapPointsToLines <-  function(points, lines, maxDist = NA, withAttrs = TRUE, id
   if (rgeosStatus()) {
     if (!requireNamespace("rgeos", quietly = TRUE))
       stop("package rgeos required for snapPointsToLines")
-  }
-  else stop("rgeos not installed")
+  } else stop("rgeos not installed")
   if (is(points, "SpatialPointsDataFrame") == FALSE && missing(withAttrs))
     withAttrs = FALSE
   if (is(points, "SpatialPointsDataFrame") == FALSE && withAttrs == TRUE)
@@ -71,26 +70,24 @@ snapPointsToLines <-  function(points, lines, maxDist = NA, withAttrs = TRUE, id
     distToPoint <- apply(d, 1, min, na.rm = TRUE)
     validLines <- distToPoint <= maxDist
     points <- points[validPoints, ]
-    lines = lines[validLines, ]
-    d = d[validLines, validPoints, drop = FALSE]
+    lines <- lines[validLines, ]
+    d <- d[validLines, validPoints, drop = FALSE]
     distToLine <- distToLine[validPoints]
     if (!any(validPoints)) {
       if (is.na(idField)) {
         idCol = character(0)
-      }
-      else {
+      } else {
         idCol = lines@data[, idField][0]
       }
       newCols = data.frame(nearest_line_id = idCol, snap_dist = numeric(0))
-      if (withAttrs)
+      if (withAttrs) {
         df <- cbind(points@data, newCols)
-      else df <- newCols
+      } else df <- newCols
       res <- SpatialPointsDataFrame(points, data = df,
                                     proj4string = CRS(proj4string(points)), match.ID = FALSE)
       return(res)
     }
-  }
-  else {
+  } else {
     distToLine = apply(d, 2, min, na.rm = TRUE)
   }
   nearest_line_index = apply(d, 2, which.min)
@@ -100,15 +97,14 @@ snapPointsToLines <-  function(points, lines, maxDist = NA, withAttrs = TRUE, id
                                                                        coordsPoints[x, ]), FUN.VALUE = c(0, 0))
   if (!is.na(idField)) {
     nearest_line_id = lines@data[, idField][nearest_line_index]
-  }
-  else {
+  } else {
     nearest_line_id = sapply(slot(lines, "lines"),
                              function(i) slot(i, "ID"))[nearest_line_index]
   }
-  if (withAttrs)
+  if (withAttrs) {
     df = cbind(points@data, data.frame(nearest_line_id, snap_dist = distToLine))
-  else df = data.frame(nearest_line_id, snap_dist = distToLine,
-                       row.names = names(nearest_line_index))
+  } else df = data.frame(nearest_line_id, snap_dist = distToLine,
+                         row.names = names(nearest_line_index))
   SpatialPointsDataFrame(coords = t(mNewCoords), data = df,
                          proj4string = CRS(proj4string(points)))
 }
@@ -122,8 +118,8 @@ snapPointsToLines <-  function(points, lines, maxDist = NA, withAttrs = TRUE, id
 # setting_name: Setting description - DEFAULT values
 ################################################################################
 
-snapPTLsf <- function(points_sf, lines_sf, crs = 3035)
-  {
+snapPTLsf <- function(points_sf, lines_sf, max_distance, crs = 3035)
+{
   # convert points to sp object
   x <-
     points_sf %>%
@@ -136,11 +132,71 @@ snapPTLsf <- function(points_sf, lines_sf, crs = 3035)
     st_cast("LINESTRING") %>%
     as_Spatial()
 
-  output <- snapPointsToLines(points = x, lines = y, withAttrs = TRUE) %>%
+  output <- snapPointsToLines(points = x, lines = y, maxDist = max_distance) %>%
     st_as_sf() %>%
     st_transform(crs)
 
   return(output)
+}
+
+st_snap_points = function(points, lines, max_dist = 1000) {
+
+  point_data <- st_drop_geometry(points)
+
+  #n = nrow(x)
+  d = st_distance(points, lines)
+  distToLine <- apply(d, 1, min, na.rm = TRUE)
+  validPoints <- distToLine <= maxDist
+  distToPoint <- apply(d, 2, min, na.rm = TRUE)
+  validLines <- distToPoint <= maxDist
+  points <- points[validPoints, ]
+  lines <- lines[validLines, ]
+  d <- d[validPoints, validLines, drop = FALSE]
+  distToLine <- distToLine[validPoints]
+  if (!any(validPoints)) message("No points closer than threshold!")
+  nearest_line_index = apply(d, 1, which.min)
+
+  coordsLines = st_geometry(lines)
+  coordsPoints = st_geometry(points)
+
+  out = do.call(c,
+                lapply(1:nrow(points), function(i) {
+                  nrst = st_nearest_points(coordsLines[nearest_line_index[i]],
+                                           coordsPoints[i])
+                  nrst_len = st_length(nrst)
+                  nrst_mn = which.min(nrst_len)
+                  if (as.vector(nrst_len[nrst_mn]) > max_dist) return(st_geometry(x)[i])
+                  return(st_cast(nrst[nrst_mn], "POINT")[2])
+                })
+                )
+  coordsLines = st_coordinates(lines)
+  coordsPoints = st_coordinates(points)
+  mNewCoords = vapply(1:nrow(points), function(x) nearestPointOnLine(coordsLines[[nearest_line_index[x]]][[1]],
+                                                                       coordsPoints[x, ]), FUN.VALUE = c(0, 0))
+  st_as_sf(out) %>%
+    bind_cols(point_data) %>%
+    return()
+}
+
+nearestPointOnLine <- function(coordsLine, coordsPoint)
+{
+  nearest_points = vapply(2:nrow(coordsLine), function(x) nearestPointOnSegment(coordsLine[(x - 1):x, ],
+                                                                                coordsPoint), FUN.VALUE = c(0, 0, 0))
+  nearest_points[1:2, which.min(nearest_points[3, ])]
+}
+
+nearestPointOnSegment <- function(s, p)
+{
+  ap = c(p[1] - s[1, 1], p[2] - s[1, 2])
+  ab = c(s[2, 1] - s[1, 1], s[2, 2] - s[1, 2])
+  t = sum(ap * ab)/sum(ab * ab)
+  t = ifelse(t < 0, 0, ifelse(t > 1, 1, t))
+  t = ifelse(is.na(t), 0, t)
+  x = s[1, 1] + ab[1] * t
+  y = s[1, 2] + ab[2] * t
+  result = c(x, y, sqrt((x - p[1])^2 + (y - p[2])^2))
+  names(result) = c("X", "Y", "distance")
+  result
 }
 
 ################################################################################
