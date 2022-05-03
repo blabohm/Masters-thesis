ba60ead8d4e639e02705054c43eee4d51ca6b32e
blabohm <benjamin.labohm@gmail.com>
2022-05-03 17:35:58 +0200

    Update 4-1-a_Scenario-1_unlimited_access.R

--- tool/Module 4 - visualisation/4-1-a_Scenario-1_unlimited_access.R
+++ tool/Module 4 - visualisation/4-1-a_Scenario-1_unlimited_access.R
@@ -10,14 +10,14 @@ getwd() %>%
 
 wd <- "C:/Users/labohben/Desktop/DE008/"
 id <- "23473-DE008L2"
-
+d <- 1000
 nodes <- paste0(wd, "nodes.gpkg")
 edges <- paste0(wd, "edges.gpkg")
 
 # ACTUAL PARK ENTRIES
 lvp_query <- paste0("SELECT * FROM nodes WHERE identifier IS '", id, "'")
 lvp_entries <- read_sf(nodes, query = lvp_query)
-lvp_filter <- st_buffer(lvp_entries, 500) %>% st_geometry() %>% st_as_text()
+lvp_filter <- st_buffer(lvp_entries, d) %>% st_geometry() %>% st_as_text()
 
 # PARK ENTRIES EVERY 2M
 gs_dir <- paste0(wd, "DE008L2_LEIPZIG_UA2018_v012.gpkg")
@@ -34,4 +34,25 @@ lvp_outline <- wd %>%
 n <-  (st_length(lvp_outline) / 5) %>% round() %>% as.numeric()
 
 # BLEND NEW PARK ENTRIES INTO NETWORK
-new_gse <- st_sample(gs, size = n, type = "regular") %>% st_cast("POINT")
+new_gse <- lvp_outline %>%
+  st_sample(size = n, type = "regular") %>%
+  st_cast("POINT") %>%
+  st_as_sf() %>%
+  mutate(area = gs$area) %>%
+  rename(geom = x)
+
+net <- edges %>%
+  read_sf(wkt_filter = lvp_filter) %>%
+  as_sfnetwork() %>%
+  convert(to_undirected) %>%
+  st_network_blend(new_gse) %>%
+  activate("edges") %>%
+  st_as_sf()
+
+#CALC INDICES
+be <- read_sf(nodes, wkt_filter = lvp_filter) %>%
+  filter(population > 0)
+
+out <- add_params(build_entries = be, gs_entries = new_gse, network = net)
+
+
