2b10b4ede35e5b65c5552dab5303264bcfc66c56
blabohm <49242340+blabohm@users.noreply.github.com>
2022-05-04 10:34:21 +0200

    Update 4-1-b_Scenario-3_population_increase.R

--- tool/Module 4 - visualisation/4-1-b_Scenario-3_population_increase.R
+++ tool/Module 4 - visualisation/4-1-b_Scenario-3_population_increase.R
@@ -4,11 +4,13 @@ library(sf)
 library(sfnetworks)
 library(ggplot2)
 library(tidygraph)
+library(stringr)
 getwd() %>%
   paste0("/tool/Module 3 - index building/functions/") %>%
   list.files(pattern = "3.*\\.R", full.names = TRUE) %>%
   for (file in .) source(file)
 
+wd <- "D:/output/DE008/"
 wd <- "C:/Users/labohben/Desktop/DE008/"
 id <- "23473-DE008L2"
 d <- 1000
@@ -21,41 +23,40 @@ lvp_entries <- read_sf(nodes, query = lvp_query)
 lvp_filter <- st_buffer(lvp_entries, d) %>% st_geometry() %>% st_as_text()
 
 # REPLACE PARK ENTRIES WITH BUILDING ENTRIES
-target_ids <- c("23502-DE008L2", "23493-DE008L2", "23485-DE008L2")
-target_query <- paste0("SELECT * FROM nodes WHERE identifier = '",
-                       target_ids, "'")
-for(q in target_query) { tmp <- read_sf(nodes, query = q)
-if (q == first(target_query)) target_gs <- tmp else target_gs <- bind_rows(target_gs, tmp) }
-
-ua_dir <- "Z:/input/UA2018/DE008L2_LEIPZIG_UA2018_v013/Data/DE008L2_LEIPZIG_UA2018_v013.gpkg"
+ua_dir <- paste0(wd, "DE008L2_LEIPZIG_UA2018_v013.gpkg")
 ua_lyr <-  st_layers(ua_dir)$name[1]
 
-pop_query <- paste0("SELECT Pop2018, area FROM ", ua_lyr, " WHERE code_2018 = 11100")
-hd_pop <- read_sf(ua_dir, wkt_filter = lvp_filter,
-                  query = pop_query)
-hd_pop <- hd_pop %>%
-  mutate(pop_per_m = Pop2018 / area)
-pop_per_m_95 <- quantile(hd_pop$pop_per_m, probs = .95)
 
-new_building_entries <- target_gs %>%
-  group_by(identifier) %>%
-  mutate(population = round(area * pop_per_m_95 / n()),
-         ID = identifier,
-         area = NA,
-         identifier = NA)
+ua_pop <- read_sf(ua_dir, wkt_filter = lvp_filter, layer = ua_lyr) %>%
+  filter(grepl("^11",  code_2018)) %>%
+  st_drop_geometry()
+hd_pop <- ua_pop %>%
+  mutate(pop_per_m = Pop2018 / area) %>%
+  group_by(code_2018) %>%
+  summarise(pop_high = quantile(pop_per_m, .95))
+
 
 # BLEND NEW BUILDINGS TO NETWORK
 #CALC INDICES
 be <- read_sf(nodes, wkt_filter = lvp_filter) %>%
   filter(population > 0) %>%
-  bind_rows(new_building_entries)
+  select(-c(area)) %>%
+  mutate(identifier = str_extract(ID, ".*L2")) %>%
+  left_join(ua_pop) %>%
+  left_join(hd_pop) %>%
+  mutate(Pop2018_new = area * pop_high,
+         population_new = round(population / Pop2018 * Pop2018_new)) %>%
+  transmute(identifier = NA,
+            area = NA,
+            population = population_new,
+            ID,
+            geom)
 
 new_gse <- read_sf(nodes, wkt_filter = lvp_filter) %>%
-  filter(!is.na(area),
-         !(identifier %in% target_ids))
-
+  filter(!is.na(area))
+net <- read_sf(edges, wkt_filter = lvp_filter)
 out <- add_params(build_entries = be, gs_entries = new_gse, network = net)
-out_dir <- paste0(wd, "scenario2/")
+out_dir <- paste0(wd, "scenario3/")
 dir.create(out_dir)
 write_output(out, network = net, out_dir = out_dir, ID = id)
 
