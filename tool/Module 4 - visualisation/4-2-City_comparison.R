230e98e045a8048f1bc138da7a787f9541027db5
blabohm <benjamin.labohm@gmail.com>
2022-06-08 17:05:35 +0200

    writing network blend + index building

--- tool/Module 4 - visualisation/4-2-City_comparison.R
+++ tool/Module 4 - visualisation/4-2-City_comparison.R
@@ -7,26 +7,29 @@ wd <- "Z:/output/"
 cities <- list.files(wd)
 out <- tibble()
 #city <- cities[1]
+nuts <- read_sf("Z:/nuts/NUTS_RG_20M_2021_3035.gpkg") %>%
+  filter(LEVL_CODE == 0) %>%
+  select(NUTS_ID)
 
 for (city in cities) {
   print(paste(which(cities == city), "of", length(cities)))
   try({
-  out <- wd %>%
-  paste0(city, "/detour_index.gpkg") %>%
-    read_sf() %>%
-    st_drop_geometry() %>%
-    mutate(city = city,
-           di = case_when(di > 1 ~ 1,
-                          #is.na(di) ~ 0,
-                          TRUE ~ di)) %>%
-    arrange(di) %>%
-    mutate(pop_cum = cumsum(population),
-           pop_sum = sum(population),
-           pop_rel = pop_cum / pop_sum,
-           di = round(di, digits = 2)) %>%
-    group_by(di) %>%
-    summarise(pop = mean(pop_rel), city = first(city)) %>%
-    bind_rows(out)
+    out <- wd %>%
+      paste0(city, "/detour_index.gpkg") %>%
+      read_sf() %>%
+      st_drop_geometry() %>%
+      mutate(city = city,
+             di = case_when(di > 1 ~ 1,
+                            #is.na(di) ~ 0,
+                            TRUE ~ di)) %>%
+      arrange(di) %>%
+      mutate(pop_cum = cumsum(population),
+             pop_sum = sum(population),
+             pop_rel = pop_cum / pop_sum,
+             di = round(di, digits = 2)) %>%
+      group_by(di) %>%
+      summarise(pop = mean(pop_rel), city = first(city)) %>%
+      bind_rows(out)
   })}
 
 #write.csv(out, "Z:/MA/di_per_pop_cum1.csv")
@@ -37,6 +40,16 @@ city_info <- read.csv("Z:/city_info.csv") %>%
   mutate(city_code = substr(URAU_COD_1, 1, 5),
          URAU_NAME = ifelse(city_code == "XK003", "Mitrovica", URAU_NAME))
 perc_coverage <- read.csv("Z:/MA/percent_OSM_coverage.csv")
+
+pop_top20 <- out %>%
+  filter(di >= .8) %>%
+  group_by(city) %>%
+  mutate(max_pop = max(pop),
+         pop_at_di80 = min(pop)) %>%
+  summarise(max_pop = first(max_pop),
+            pop_at_di80 = first(pop_at_di80)) %>%
+  mutate(top20 = max_pop - pop_at_di80) %>%
+  rename(city_code = city)
 
 city_sf <- read_sf(city_boundaries) %>%
   rename(city_code = URAU_CODE) %>%
@@ -49,18 +62,40 @@ plot_df <- city_sf %>%
   right_join(rename(out, "city_code" = "city"))
 
 plot_sf <- out %>%
-  mutate(di = ifelse(is.na(di), 0, di),
-         pop_di = di * pop) %>%
+  mutate(di = ifelse(is.na(di), 0, di)) %>%
   group_by(city) %>%
-  summarise(di = mean(di), pop = mean(pop), pop_di = mean(pop_di)) %>%
+  summarise(di = mean(di), pop = max(pop)) %>%
   rename(city_code = city) %>%
   inner_join(city_sf) %>%
-  st_as_sf()
+  left_join(pop_top20) %>%
+  st_as_sf() %>%
+  arrange(top20)
 
 ggplot() +
   geom_sf(data = plot_sf, aes(fill = di, color = di))
 ggplot() +
-  geom_sf(data = plot_sf, aes(fill = pop_di, color = pop_di))
+  geom_sf(data = plot_sf, aes(fill = top20, color = top20))
+ggplot() +
+  geom_sf(data = plot_sf, aes(fill = pop, size = di))
+
+plot_cent <- st_point_on_surface(plot_sf)
+
+city_labs <- filter(plot_cent, grepl("001|NL002", city_code)) %>%
+  mutate(URAU_NAME = ifelse(grepl("NL002", city_code), "Amsterdam", URAU_NAME)) %>%
+  filter(city_code != "NL001") %>% select(URAU_NAME)
+ggplot() +
+  geom_sf(data = nuts, fill = "gray75") +
+  # geom_sf(data = plot_cent, aes(fill = top20, color = top20,
+  #                               size = pop)#, alpha = .8
+  # ) +
+  geom_sf(data = plot_cent, aes(fill = pop, color = pop,
+                                     size = top20)#, alpha = .8
+  ) +
+  coord_sf(xlim = c(2700000, 5748970),
+           ylim = c(1500000, 4500000)) +
+  geom_sf_text(data = city_labs, aes(label = URAU_NAME), color = "white",
+               nudge_x = -50000, nudge_y = -50000, check_overlap = TRUE) +
+  theme_dark()
 
 out_cntr <- plot_df %>%
   group_by(CNTR_CODE, di) %>%
@@ -75,16 +110,27 @@ out %>%
   na.omit() %>%
   ggplot(aes(x = di, y = pop, col = city)) + geom_line()
 
-install.packages("ggrepel")
 library(ggrepel)
 data_ends <- out_cntr %>% group_by(CNTR_CODE) %>% summarise(pop = last(pop), di = last(di))
 out_cntr %>%
   na.omit() %>%
+  arrange(pop) %>%
   ggplot(aes(x = di, y = pop, col = CNTR_CODE)) +
-  geom_line() +
+  geom_line(size = 1) +
+  scale_color_viridis_d() +
   theme(legend.position = "none") +
-  geom_label_repel(aes(label = paste0(CNTR_CODE, ":", round(pop, digits = 2))),
-                   data = data_ends, max.overlaps = 70)
+  geom_label_repel(aes(label = #paste0(CNTR_CODE, ":", round(pop, digits = 2))
+                       CNTR_CODE
+                       ),
+                   data = data_ends,
+                   max.overlaps = 5,
+                   nudge_x = .1,
+                   size = 3,
+                   direction = "y",
+                   hjust = 0,
+                   force = 0.5, force_pull = 0.1
+                   ) +
+  xlim(c(.4, 1))
 
 out_region %>%
   na.omit() %>%
@@ -94,8 +140,9 @@ plot_ly(data = out, x = ~di, y = ~pop, color = ~city) %>%
   add_lines()
 plot_ly(data = out_cntr, x = ~di, y = ~pop, color = ~CNTR_CODE) %>%
   add_lines()
-out_sav <- out
-out <- out_sav
+
+# max pop - pop wert bei .8
+
 # ls <- read_sf("C:/project/DE001/local_significance.gpkg") %>%
 #   filter(!is.na(ls)) %>%
 #   mutate(edge_length = st_length(.)) %>%
