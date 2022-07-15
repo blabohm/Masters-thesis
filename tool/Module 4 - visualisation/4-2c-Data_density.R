eb097ed1a5f85f120f60ad2983e3a2b417b0187c
blabohm <benjamin.labohm@gmail.com>
2022-07-15 19:30:03 +0200

    osm cov cities

--- tool/Module 4 - visualisation/4-2c-Data_density.R
+++ tool/Module 4 - visualisation/4-2c-Data_density.R
@@ -3,85 +3,85 @@ library(sf)
 library(dplyr)
 library(RColorBrewer)
 library(ggspatial)
+library(cowplot)
 
 wd <- "Z:/output/"
 wd1 <- "Z:/input/"
 ua <- "Z:/input/UA2018/"
-codes <- c("PT001", "AL001")
+codes <- c("PT001", "AT001")
 
-edges <- paste0(wd, code, "/edges.gpkg")
-gs_q <- paste0("SELECT area, geom, identifier FROM ",  st_layers(ua_dir)$name[1],
-               " WHERE code_2018 is 14100 OR code_2018 is 31000")
+code <- codes[1]
 
 build_poly <- paste0(wd, code, "/buildings.gpkg")
 
-bbox <- read_sf(paste0(wd1, code, "_cov.gpkg")) %>% st_bbox()
-bbox_filter <- bbox %>% st_as_sfc() %>% st_as_text()
+boundaries <- paste0(wd1, "cities.gpkg")
+ua_q_bound <- paste0("SELECT * FROM cities WHERE URAU_CODE = '", code, "'")
+bound <- read_sf(boundaries, query = ua_q_bound)
+bound_filter <- bound %>% st_as_sfc() %>% st_as_text()
+
+# bbox <- read_sf(paste0(wd1, code, "_cov.gpkg")) %>% st_bbox()
+# bbox_filter <- bbox %>% st_as_sfc() %>% st_as_text()
 
 ua_dir <- list.files(ua, pattern = code, full.names = TRUE) %>%
   list.files(pattern = ".gpkg$", recursive = TRUE, full.names = TRUE)
 ua_q <- paste0("SELECT * FROM ",  st_layers(ua_dir)$name[1])
-ua <- read_sf(ua_dir, query = ua_q, wkt_filter = bbox_filter)
+#ua_data <- read_sf(ua_dir, query = ua_q, wkt_filter = bbox_filter)
+ua_data_full <- read_sf(ua_dir, query = ua_q, wkt_filter = bound_filter)
 
-gs <- filter(ua, code_2018 %in% c(14100, 31000))
-res <- filter(ua, grepl("^11.*", code_2018))
-buildings <- read_sf(build_poly, wkt_filter = bbox_filter) %>% select(geom)
 
-xmin <- bbox[1] + 2100
-xmax <- bbox[3] - 2100
-ymin <- bbox[2] + 2100
-ymax <- bbox[4] - 2100
+buildings_full <- read_sf(build_poly, wkt_filter = bound_filter) %>% select(geom)
+res_full <- filter(ua_data_full, grepl("^11.*", code_2018))
+cov_res_full <- st_filter(res_full, buildings_full)
+not_cov_res_full <- filter(res_full, !(identifier %in% cov_res_full$identifier))
 
-build_plot <- ggplot() +
-  geom_sf(data = res, color = "red3", fill = "red3") +
-  geom_sf(data = buildings, color = "black") +
-  coord_sf(xlim = c(xmin, xmax),
-           ylim = c(ymin, ymax))
-build_plot
+ggplot() +
+  geom_sf(data = bound, color = "grey30", fill = "grey70") +
+  geom_sf(data = cov_res_full, color = "red3", fill = "red3") +
+  geom_sf(data = not_cov_res_full, color = "grey50", fill = "grey50")
 
-base_plot <- ggplot() +
-  geom_sf(data = st_buffer(st_as_sfc(bbox), 1000), fill = "gray93") +
-  geom_sf(data = buildings, fill = "gray99", color = "gray99") +
-  geom_sf(data = gs, fill = "gray85", color = "gray85") +
-  geom_sf(data = read_sf(edges, wkt_filter = bbox_filter) %>% select(geom),
-          color = "gray80", size = 1)
+# base_plot <- ggplot() +
+#   geom_sf(data = st_buffer(st_as_sfc(bbox), 1000), fill = "gray93") +
+#   geom_sf(data = buildings, fill = "gray99", color = "gray99") +
+#   geom_sf(data = gs, fill = "gray85", color = "gray85") +
+#   geom_sf(data = read_sf(edges, wkt_filter = bbox_filter) %>% select(geom),
+#           color = "gray80", size = 1)
 
 base_plot
 
-
-################################################################################
-ls_query <- paste0("SELECT * FROM local_significance WHERE ls is not null")
-ls_values <- read_sf(paste0(wd, code, "/local_significance.gpkg"),
-                     query = ls_query, wkt_filter = bbox_filter)
-ls_plot <- base_plot +
-  ls_values %>%
-  select(ls) %>%
-  filter(!is.na(ls)) %>%
-  mutate(ls = log(ls)) %>%
-  arrange(ls) %>%
-  geom_sf(data = ., aes(color = ls), size = 2) +
-  scale_color_distiller(palette = "RdBu") +
-  coord_sf(xlim = c(xmin, xmax),
-           ylim = c(ymin, ymax)) +
-  labs(color = "LS") +
-  annotation_scale(aes(style = "ticks"))
-ls_plot
-
-################################################################################
-
-di_query <- paste0("SELECT * FROM detour_index WHERE di is not null")
-di_values <- read_sf(paste0(wd, code, "/detour_index.gpkg"),
-                     query = di_query, wkt_filter = bbox_filter)
-
-di_plot <- base_plot +
-  di_values %>%
-  select(di) %>%
-  filter(!is.na(di)) %>%
-  geom_sf(data = ., aes(fill = di, color = di)) +
-  scale_fill_distiller(palette = "RdBu") +
-  scale_color_distiller(palette = "RdBu") +
-  coord_sf(xlim = c(xmin, xmax),
-           ylim = c(ymin, ymax)) +
-  labs(fill = "DI", color = "DI") +
-  annotation_scale(aes(style = "ticks"))
-di_plot
+#
+# ################################################################################
+# ls_query <- paste0("SELECT * FROM local_significance WHERE ls is not null")
+# ls_values <- read_sf(paste0(wd, code, "/local_significance.gpkg"),
+#                      query = ls_query, wkt_filter = bbox_filter)
+# ls_plot <- base_plot +
+#   ls_values %>%
+#   select(ls) %>%
+#   filter(!is.na(ls)) %>%
+#   mutate(ls = log(ls)) %>%
+#   arrange(ls) %>%
+#   geom_sf(data = ., aes(color = ls), size = 2) +
+#   scale_color_distiller(palette = "RdBu") +
+#   coord_sf(xlim = c(xmin, xmax),
+#            ylim = c(ymin, ymax)) +
+#   labs(color = "LS") +
+#   annotation_scale(aes(style = "ticks"))
+# ls_plot
+#
+# ################################################################################
+#
+# di_query <- paste0("SELECT * FROM detour_index WHERE di is not null")
+# di_values <- read_sf(paste0(wd, code, "/detour_index.gpkg"),
+#                      query = di_query, wkt_filter = bbox_filter)
+#
+# di_plot <- base_plot +
+#   di_values %>%
+#   select(di) %>%
+#   filter(!is.na(di)) %>%
+#   geom_sf(data = ., aes(fill = di, color = di)) +
+#   scale_fill_distiller(palette = "RdBu") +
+#   scale_color_distiller(palette = "RdBu") +
+#   coord_sf(xlim = c(xmin, xmax),
+#            ylim = c(ymin, ymax)) +
+#   labs(fill = "DI", color = "DI") +
+#   annotation_scale(aes(style = "ticks"))
+# di_plot
